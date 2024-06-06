9633af106cf4c7a05ee3e557ab1c449cacaf8d54
BlackDemise <anhsohackervl@gmail.com>
2024-06-06 19:31:25 +0700

    fix: he186453 Move constants defined in this interfaces to another class or enum

--- src/main/java/org/ims/constant/ICandidateQuery.java
+++ src/main/java/org/ims/constant/ICandidateQuery.java
@@ -1,23 +1,23 @@
 package org.ims.constant;
 
-public interface ICandidateQuery {
-    String GET_CANDIDATES = "SELECT\n"
+public class ICandidateQuery {
+    final String GET_CANDIDATES = "SELECT\n"
             + "*\n"
             + "FROM Candidate\n"
             + "WHERE candidate_name LIKE ?";
 
-    String GET_ONE_CANDIDATE = "SELECT\n"
+    final String GET_ONE_CANDIDATE = "SELECT\n"
             + "*\n"
             + "FROM Candidate\n"
             + "WHERE candidate_id = ?\n";
 
-    String INSERT_A_NEW_CANDIDATE = "INSERT INTO Candidate\n"
+    final String INSERT_A_NEW_CANDIDATE = "INSERT INTO Candidate\n"
             + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)\n";
 
-    String DELETE_A_CANDIDATE = "DELETE FROM Candidate\n"
+    final String DELETE_A_CANDIDATE = "DELETE FROM Candidate\n"
             + "WHERE candidate_id = ?\n";
 
-    String UPDATE_A_CANDIDATE = "UPDATE Candidate\n"
+    final String UPDATE_A_CANDIDATE = "UPDATE Candidate\n"
             + "SET candidate_name = ?,\n"
             + "    candidate_address = ?,\n"
             + "    candidate_phone_number = ?,\n"
