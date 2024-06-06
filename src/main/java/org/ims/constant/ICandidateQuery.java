8f108dbe68160ab2d5322cc902e2123fa566b0e9
BlackDemise <anhsohackervl@gmail.com>
2024-06-06 19:36:02 +0700

    fix: he186453 redo:Move constants defined in this interfaces to another class or enum

--- src/main/java/org/ims/constant/ICandidateQuery.java
+++ src/main/java/org/ims/constant/ICandidateQuery.java
@@ -1,23 +1,23 @@
 package org.ims.constant;
 
 public class ICandidateQuery {
-    final String GET_CANDIDATES = "SELECT\n"
+    public static final String GET_CANDIDATES = "SELECT\n"
             + "*\n"
             + "FROM Candidate\n"
             + "WHERE candidate_name LIKE ?";
 
-    final String GET_ONE_CANDIDATE = "SELECT\n"
+    public static final String GET_ONE_CANDIDATE = "SELECT\n"
             + "*\n"
             + "FROM Candidate\n"
             + "WHERE candidate_id = ?\n";
 
-    final String INSERT_A_NEW_CANDIDATE = "INSERT INTO Candidate\n"
+    public static final String INSERT_A_NEW_CANDIDATE = "INSERT INTO Candidate\n"
             + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)\n";
 
-    final String DELETE_A_CANDIDATE = "DELETE FROM Candidate\n"
+    public static final String DELETE_A_CANDIDATE = "DELETE FROM Candidate\n"
             + "WHERE candidate_id = ?\n";
 
-    final String UPDATE_A_CANDIDATE = "UPDATE Candidate\n"
+    public static final String UPDATE_A_CANDIDATE = "UPDATE Candidate\n"
             + "SET candidate_name = ?,\n"
             + "    candidate_address = ?,\n"
             + "    candidate_phone_number = ?,\n"
